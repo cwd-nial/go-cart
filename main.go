af800a0d7bc7e20bc1cb2726a95ebb5d2147e07d
Niels Albarran <niels.albarran@limango.de>
2019-09-27 15:17:58 +0200

    added the webserver

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/cwd-nial/go-cart/service"
 )
 
 var appName = "cartservice"
 
 func main() {
 	fmt.Printf("Starting %v\n", appName)
+	service.StartWebServer("6767")
 }
